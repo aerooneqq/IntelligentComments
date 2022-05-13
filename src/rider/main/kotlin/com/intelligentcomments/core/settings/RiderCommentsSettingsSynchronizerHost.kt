86c681823fc62ef028d48ddc6a9d7156e4bb164b
aero <aerooneQ@yandex.ru>
2022-05-13 15:40:41 +0300

    Fixed not handling multiple hosts

--- src/rider/main/kotlin/com/intelligentcomments/core/settings/RiderCommentsSettingsSynchronizerHost.kt
+++ src/rider/main/kotlin/com/intelligentcomments/core/settings/RiderCommentsSettingsSynchronizerHost.kt
@@ -4,14 +4,19 @@ import com.intellij.util.application
 import com.jetbrains.rd.ide.model.rdCommentsSettingsModel
 import com.jetbrains.rd.ide.model.shellModel
 import com.jetbrains.rd.platform.util.lifetime
+import com.jetbrains.rd.util.reactive.AddRemove
 import com.jetbrains.rd.util.reactive.flowInto
 import com.jetbrains.rider.protocol.protocolManager
 
 class RiderCommentsSettingsSynchronizerHost {
   init {
-    val model = application.protocolManager.protocolHosts.first().protocol.shellModel.rdCommentsSettingsModel
-    val settingsProvider = RiderIntelligentCommentsSettingsProvider.getInstance()
-    model.enableExperimentalFeatures.set(settingsProvider.useExperimentalFeatures.value)
-    settingsProvider.useExperimentalFeatures.flowInto(application.lifetime, model.enableExperimentalFeatures)
+    application.protocolManager.protocolHosts.advise(application.lifetime) { addRemove, host ->
+      if (addRemove == AddRemove.Add) {
+        val model = host.protocol.shellModel.rdCommentsSettingsModel
+        val settingsProvider = RiderIntelligentCommentsSettingsProvider.getInstance()
+        model.enableExperimentalFeatures.set(settingsProvider.useExperimentalFeatures.value)
+        settingsProvider.useExperimentalFeatures.flowInto(application.lifetime, model.enableExperimentalFeatures)
+      }
+    }
   }
 }
