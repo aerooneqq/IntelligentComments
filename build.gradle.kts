45e925405c4b5251ce9fddd51472a7208a21c770
aero <aerooneq@yandex.ru>
2022-09-05 16:38:13 +0300

    Promote idea plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     id("java")
     id("me.filippov.gradle.jvm.wrapper") version "0.10.0"
     id("org.jetbrains.kotlin.jvm") version "1.6.21"
-    id("org.jetbrains.intellij") version "1.7.0"
+    id("org.jetbrains.intellij") version "1.9.0"
 }
 
 apply {
