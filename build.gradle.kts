774665b87f677db0e5fe06d5db7338176cbdf0e5
aero <aerooneQ@yandex.ru>
2024-09-24 00:06:15 +0300

    Updating sdks

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,13 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.jetbrains.rd:rd-gen:2023.2.1")
+        classpath("com.jetbrains.rd:rd-gen:2024.1.1")
     }
 }
 
 plugins {
     id("me.filippov.gradle.jvm.wrapper") version "0.14.0"
-    id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    id("org.jetbrains.kotlin.jvm") version "2.0.0"
     id("org.jetbrains.intellij") version "1.13.3"
 }
 
